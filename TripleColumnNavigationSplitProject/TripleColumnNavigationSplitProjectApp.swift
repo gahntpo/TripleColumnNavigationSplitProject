031cb87a21f6765c2a4ca6841f6d8c441295702a
Karin Prater <gahntpo@yahoo.de>
2022-11-13 11:11:09 +0200

    Initial Commit

---
+++ TripleColumnNavigationSplitProject/TripleColumnNavigationSplitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripleColumnNavigationSplitProjectApp.swift
+//  TripleColumnNavigationSplitProject
+//
+//  Created by Karin Prater on 13.11.22.
+//
+
+import SwiftUI
+
+@main
+struct TripleColumnNavigationSplitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
